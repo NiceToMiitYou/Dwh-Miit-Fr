61c7526440bc05a7cf5eab956f9c292f50d55645
Boris Tacyniak <boris.tacyniak@itevents.fr>
2014-12-04 00:09:59 +0100

    Fix tests

---
+++ tasks/config/test.js
@@ -0,0 +1,20 @@
+/**
+ *
+ *
+ */
+module.exports = function( grunt ) {
+
+    grunt.config.set( 'mochaTest', {
+        test: {
+            options: {
+
+                reporter: 'spec'
+
+            },
+
+            src: [ 'test/**/*.test.js' ]
+        }
+    } );
+
+    grunt.loadNpmTasks( 'grunt-mocha-test' );
+};
