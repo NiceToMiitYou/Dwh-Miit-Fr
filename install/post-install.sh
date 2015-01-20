c2a11fb782ef6fa41fcef91ebff4f8f3f678e105
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-01-20 01:04:11 +0100

    [FIX] fix post install database installation

--- install/post-install.sh
+++ install/post-install.sh
@@ -4,17 +4,7 @@ if [ -f /.dockerinit ]; then
 
     echo "Info: I'm inside matrix, so I will try to reset the world!";
 
-    DATABASE="fr_miit_dwh"
-    RESULT=$(mysqlshow --u root -p $DATABASE| grep -v Wildcard | grep -o $DATABASE)
-
-    # Database installed:
-    if [ "$RESULT" == "$DATABASE" ]; then
-        
-        echo "Info: database already installed."
-    else
-
-        mysql -u root < install/database.sql
-    fi
+    mysql -u root < install/database.sql
 else
 
     echo "Info: Outside of the container, the database must be installed manualy."
