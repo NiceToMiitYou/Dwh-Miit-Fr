2884693cf93930378ae597fdbd4584b1ef86ce75
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-03-18 22:30:32 +0100

    [ENHACEMENT] Production configuration

--- config/env/production.js
+++ config/env/production.js
@@ -14,23 +14,41 @@ module.exports = {
 
     application: {
 
-        sqs: 'prd-miit-fr'
+        sqs: 'prd-miit-fr',
+        s3:  'cdn.miit.fr'
     },
 
     connections: {
 
         DwhMySQL: {
 
-            host: 'localhost',
-            user: 'root',
-            password: '',
-            database: 'fr_miit_dwh'
+            host:     process.env.MIIT_MYSQL_HOST,
+            port:      3306,
+            user:     process.env.MIIT_MYSQL_USER,
+            password: process.env.MIIT_MYSQL_PASS,
+            database: process.env.MIIT_MYSQL_DB
         }
+    },
+
+    session: {
+        adapter: 'redis',
+        host:    'miit-fr-001.xidhqo.0001.euc1.cache.amazonaws.com',
+        port:     6379,
+        prefix:  'sess:',
+        db:      'dwh-miit-fr'
+    },
+
+    sockets: {
+        adapter: 'redis',
+        host:    'miit-fr-001.xidhqo.0001.euc1.cache.amazonaws.com',
+        port:     6379,
+        prefix:  'sock:',
+        db:      'dwh-miit-fr'
     },
 
     port: 80,
   
     log: {
-        level: "silent"
+        level: 'silent'
     }
 };
