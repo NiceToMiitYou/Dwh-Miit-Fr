3b5a50b29809fb3aca48822c3464299ba0691230
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-03-19 16:50:06 +0100

    [FIX] Missing ,

--- config/env/production.js
+++ config/env/production.js
@@ -43,7 +43,7 @@ module.exports = {
     },
 
     sockets: {
-        _hookTimeout:  1000000
+        _hookTimeout:  1000000,
         adapter:      'socket.io-redis',
         host:         'miit-fr-001.xidhqo.0001.euc1.cache.amazonaws.com',
         port:          6379,
