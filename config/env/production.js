73767c2369f5fd2f720fe519b65dae48db97dc04
Boris Tacyniak <boris.tacyniak@itevents.fr>
2015-03-25 22:42:47 +0100

    [FIX] fix redis server

--- config/env/production.js
+++ config/env/production.js
@@ -36,7 +36,7 @@ module.exports = {
 
     session: {
         adapter: 'redis',
-        host:    'miit-fr-001.xidhqo.0001.euc1.cache.amazonaws.com',
+        host:    'miit-fr.xidhqo.0001.euc1.cache.amazonaws.com',
         port:     6379,
         prefix:  'sess:',
         db:      'dwh-miit-fr'
@@ -45,7 +45,7 @@ module.exports = {
     sockets: {
         _hookTimeout:  1000000,
         adapter:      'socket.io-redis',
-        host:         'miit-fr-001.xidhqo.0001.euc1.cache.amazonaws.com',
+        host:         'miit-fr.xidhqo.0001.euc1.cache.amazonaws.com',
         port:          6379,
         prefix:       'sock:',
         db:           'dwh-miit-fr'
